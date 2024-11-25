c1534a430576ff4b3c15bbf3bdfbf1c10f36d242
Anthony Tabet <anthonytabet@Nino.lan>
2024-11-25 23:13:45 +0700

    intro to js

---
+++ Week 3/Monday/IntroToJS.js
@@ -0,0 +1,108 @@
+// variables
+
+const x = 1; // constant that can't be changed after creation
+
+let y = 2; // can be changed to any datatype
+y = 3;
+
+y = z + x;
+
+var z = 3;
+
+const xx = 1;
+const xy = 1
+
+// Datatypes
+const a = 1; // Int
+const aa = 1.1; // Float or Double
+
+const b = 'Hello world'; // "" or '' or ``
+
+const c = false;
+const d = true;
+
+const e = null;
+
+// objects
+
+let data = {
+    myth: 'cats have 9 lives',
+    age: 25,
+    isTeacher: true,
+    address: {
+        street: '123 street',
+        city: 'somewhere'
+    }
+}
+
+// data.isTeacher
+
+console.log(data.isTeacher); // will print 'true'
+console.log(data.address.city); // will print 'somewhere'
+
+// how to comment
+
+// asd
+/*
+asd
+*/
+
+const testArray = ['woof','meow','hello', 100, true];
+
+console.log(testArray[2]);
+console.log(testArray[3]); // what would this print to the console? - 100
+
+for (let i = 0; i<5; i++){
+    console.log('hi');
+} // will print i 5 times
+
+while (d = true){
+    osiadnfoaidnfoadnfioasn;
+    d = false;
+}
+
+let yy = 10;
+if (yy = 2){
+    console.log(data.myth);
+}else if(yy=3){
+    console.log('hello world');
+}else{
+    console.log('else statement triggered')
+}
+
+// if(){
+
+// }else{
+
+// }
+
+// if(){
+
+// }
+
+const abel = 5
+const nat = 4
+
+if((abel = 5) & (nat = 4)){
+
+}
+
+if((abel <= 5) || (nat >= 55)){
+    console.log();
+}
+
+let q = 5;
+
+console.log("The value of q is: " + q) // first solution - The value of q is: 5
+
+console.log(`The value of q is: ${q}`) // second solution - The value of q is: 5
+
+console.log(q); // will print -  5
+
+" "
+' '
+` `
+
+const arrayLength = testArray.length;
+
+// console.log(arrayLength) - will print '5'
---
+++ Week 3/Monday/functionExplination.js
@@ -0,0 +1,10 @@
+function sideQuest(y){
+    console.log('the value of y is: ' + y);
+}
+
+function main(){
+    let y = 25;
+    sideQuest(y);
+}
+
+main();
