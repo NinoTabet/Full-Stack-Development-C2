2951de0d206a0355da77b336c422706c3e219ab7
Nino <Anthonydavidtabet@gmail.com>
2024-12-17 18:43:47 +0200

    week 6 tuesday

--- Application/server/index.js
+++ Application/server/index.js
@@ -5,28 +5,127 @@ const pool = require('./db'); // -------- !!!
 
 app.use(express.json());
 
+// get request to search for movies
 app.get("/Search", async (req, res) => {
     try {
-        console.log('yes')
+        // collects movie name from request
         const {movieName} = req.query;
 
-        console.log('before query')
-        const searchMovies = await pool.query(
-            'SELECT * FROM movies WHERE movie_title = $1',
-            [movieName]
-        );
-        console.log(searchMovies);
+        // checks if movie name is undefined
+        if (!movieName){
+            return res.status(401).json({message:'Please be sure to input a movie name before you submit the form.'})
+        }
 
-        if(searchMovies.rows.length == 0){
+        // searches db for provided movie name
+        const searchMovies = await pool.query(
+            'SELECT * FROM movies WHERE movie_title ILIKE $1',
+            [`%${movieName}%`]
+        );
+
+        // checks if movie exists in db
+        if(searchMovies.rowCount == 0){
             return res.status(404).json("Movie not found");
         }
 
-        res.status(200).json(searchMovies);
+        // responds with all relevant movies
+        res.status(200).json(searchMovies.rows);
 
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({message: 'Internal Server Error'})
+    }
+})
+
+// creates movie in db
+app.post("/Create", async (req, res) => {
+    try {
+
+        // collects movie title from request body
+        const {movieTitle} = req.body;
+        console.log(movieTitle)
+
+        // checks if movie title is undefined
+        if (!movieTitle){
+            return res.status(401).json({message:'Please be sure to input a movie name before you submit the form.'})
+        }
+
+        // checks if movie exists in db
+        const movieSearch = await pool.query(
+            'SELECT * FROM movies WHERE movie_title ILIKE $1',
+            [movieTitle]
+        );
+
+        console.log(movieSearch);
+
+        // replies to use with 401 in case the movie already exists
+        if(movieSearch.rowCount > 0){
+            return res.status(401).json('Movie already exists in database.')            
+        }
+
+        // adds movie to db
+        await pool.query(
+            'INSERT INTO movies (movie_title) VALUES ($1)',
+            [movieTitle]
+        );
+
+        // 200 - movie added successfully
+        res.status(200).json({message:'Movie created successfully.'});
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({message: 'Internal Server Error'})
+    }
+})
+
+// removes movie from db
+app.delete("/Remove", async (req, res) => {
+    try {
+        // collects movie title from request
+        const {movieTitle} = req.query
+
+        // checks if movieTitle is undefined
+        if (!movieTitle){
+            return res.status(401).json({message:'Please be sure to input a movie name before you submit the form.'})
+        }
+
+        
+        // checks if movie exists in db
+        const movieSearch = await pool.query(
+            'SELECT * FROM movies WHERE movie_title ILIKE $1',
+            [movieTitle]
+        );
+
+        // replies to use with 401 in case the movie does not exist
+        if(movieSearch.rowCount == 0){
+            return res.status(401).json('Movie doesn\'t exist in database. Please choose one that exists to be able to delete it.')            
+        }
+
+        // deletes movie from db
+        await pool.query(
+            'DELETE FROM movies WHERE movie_title ILIKE $1',
+            [movieTitle]
+        )
+
+        // 200 - movie deleted successfully
+        res.status(200).json({message: 'Movie deleted successfully.'});
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({message: 'Internal Server Error'})
+    }
+})
+
+// do this first thing tomorrow --- 
+app.put("/Update", async (req, res) => {
+    try {
+
+        res.status(200).json();
     } catch (err) {
         return res.status(500).json({message: 'Internal Server Error'})
     }
 })
+
+
 
 const PORT = 3000
 app.listen(PORT, () => {
