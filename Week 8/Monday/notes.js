defde1f5454184544d5c698b2c2cb28e55159f0b
Nino <Anthonydavidtabet@gmail.com>
2025-01-20 23:06:16 +0700

    bootstrap

---
+++ Week 8/Monday/notes.js
@@ -0,0 +1,125 @@
+/*
+    background: bg-*
+
+    width:
+        w-25
+        w-50
+        w-75
+        w-100
+        w-auto
+        vw-100
+        min-vh-100
+
+    Height
+        h-25
+        h-50
+        h-75
+        h-100
+        h-auto
+        vh-100
+        min-vh-100
+
+    Colours:
+        - Primary (can also include -subtle) : Blue
+        - Secondary : Gray
+        - Success : Green
+        - Danger : Red
+        - Warning : Yellow
+        - Info : Cyan/light blue
+        - Dark : Dark gray
+        - Black : perfect black 0 0 0 
+        - Light : Grayish white
+        - White : perfect white 255 255 255
+
+    border:
+        border : gray border
+        border-*
+
+    text:
+        text-*
+        text-center
+    
+    p:
+        p- 0 -> 5 || auto
+
+        pb : bottom padding
+        ps : start padding (left)
+        pt : top padding
+        pe : end padding (right)
+        px : x axis padding
+        py : y axis padding
+
+    m:
+        m- 0 -> 5 || auto
+
+        mb : bottom margin
+        ms : start margin (left)
+        mt : top margin
+        me : end margin (right)
+        mx : x axis margin
+        my : y axis margin
+    
+    Container: container -> creates a container that sets it's max-width variably depending on screen size
+
+    Grids:
+        d-grid : display grid
+
+        row-cols-#
+
+        row
+        
+        g : gutters 0-5
+
+        col 1-12 || auto
+
+    d-flex
+
+    justify-content-start
+    justify-content-end
+    justify-content-center
+    justify-content-between
+    justify-content-around
+    justify-content-evenly
+
+    align-content-start
+    align-content-end
+    align-content-center
+    align-content-between
+    align-content-around
+    align-content-stretch
+
+    align-items-start
+    align-items-end
+    align-items-center
+    align-items-baseline
+    align-items-stretch
+
+    opacity : 0 || 25 || 50 || 75 || 100
+
+    buttons:
+
+        btn-primary 
+        btn-secondary 
+        btn-success 
+        btn-danger 
+        btn-warning 
+        btn-info 
+        btn-dark 
+        btn-black 
+        btn-light 
+        btn-white
+
+        btn-outline-primary 
+        btn-outline-secondary 
+        btn-outline-success 
+        btn-outline-danger 
+        btn-outline-warning 
+        btn-outline-info 
+        btn-outline-dark 
+        btn-outline-black 
+        btn-outline-light 
+        btn-outline-white
+
+    rounded
+    rounder-pill
+*/
